af5c7c457a4f67178a580d2134dce0f270d69d27
Amit Mondol <amit.mondol@bjitgroup.com>
2022-03-08 08:52:12 +0600

    first commit

---
+++ BackgroundDownloadIOS/Manager/DownloadManager.swift
@@ -0,0 +1,114 @@
+//
+//  DownloadManager.swift
+//  BackgroundDownloadIOS
+//
+//  Created by AMIT on 3/8/22.
+//
+
+import Foundation
+import UIKit
+
+class DownloadManager: NSObject, ObservableObject {
+    private static var downloadManager: DownloadManager!
+    
+    private var urlSession: URLSession!
+    private var tasks: [URLSessionTask] = []
+    
+    @discardableResult
+    public static func shared() -> DownloadManager {
+        if downloadManager == nil {
+            downloadManager = DownloadManager()
+        }
+        return downloadManager
+    }
+
+    private override init() {
+        super.init()
+        //let config = URLSessionConfiguration.default
+        let config = URLSessionConfiguration.background(withIdentifier: "MySession")
+        config.isDiscretionary = true
+        config.sessionSendsLaunchEvents = true
+        urlSession = URLSession(configuration: config, delegate: self, delegateQueue: nil) //OperationQueue.main
+        updateTasks()
+    }
+    
+    func startDownload(_ url: URL) {
+        let task = urlSession.downloadTask(with: url)
+        task.resume()
+        tasks.append(task)
+    }
+    
+    func startDownload(_ fileUrl: String?, _ fileName: String?) {
+        if let fileUrl = fileUrl, let url = URL(string: fileUrl) {
+            startDownload(url, fileName)
+        }
+    }
+    
+    func startDownload(_ url: URL, _ fileName: String?) {
+        let task = urlSession.downloadTask(with: url)
+        task.earliestBeginDate = Date().addingTimeInterval(1)
+        task.countOfBytesClientExpectsToSend = 200
+        task.countOfBytesClientExpectsToReceive = 500 * 1024
+        task.resume()
+        tasks.append(task)
+    }
+    
+    private func updateTasks() {
+        urlSession.getAllTasks { tasks in
+            DispatchQueue.main.async {
+                self.tasks = tasks
+            }
+        }
+    }
+}
+
+extension DownloadManager: URLSessionDelegate, URLSessionDownloadDelegate {
+    func urlSession(_: URLSession, downloadTask: URLSessionDownloadTask, didWriteData _didWriteData: Int64, totalBytesWritten _totalBytesWritten: Int64, totalBytesExpectedToWrite _totalBytesExpectedToWrite: Int64) {
+        print("Progress \(downloadTask.progress.fractionCompleted) for \(downloadTask)  \(_totalBytesWritten) \(_totalBytesExpectedToWrite)")
+    }
+    
+    func urlSession(_: URLSession, downloadTask task: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
+        print("Download finished: \(location.absoluteString)")
+        guard
+            let httpURLResponse = task.response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+            let mimeType = task.response?.mimeType else {
+                print ("Response error!");
+                return
+            }
+        DownloadManager.save((task.currentRequest?.url!)!, location, mimeType, nil)
+    }
+    
+    func urlSession(_: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
+        if let error = error {
+            print("Download error: \(String(describing: error))")
+        } else {
+            print("Task finished: \(task)")
+        }
+    }
+
+    func urlSessionDidFinishEvents(forBackgroundURLSession session: URLSession) {
+        DispatchQueue.main.async {
+            guard let appDelegate = UIApplication.shared.delegate as? AppDelegate, let backgroundCompletionHandler = appDelegate.backgroundCompletionHandler else { return }
+            backgroundCompletionHandler()
+        }
+    }
+}
+
+extension DownloadManager {
+    private static func save(_ url: URL, _ location: URL, _ mimeType: String, _ fileName: String?) {
+        do {
+            if mimeType.hasPrefix("image") {
+                guard let inputImage = UIImage(named: location.path) else { return }
+                UIImageWriteToSavedPhotosAlbum(inputImage, nil, nil, nil)
+            }else {
+                let documentsURL = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+                let savedURL = documentsURL.appendingPathComponent((fileName == nil) ? url.lastPathComponent : fileName!)
+                if FileManager.default.fileExists(atPath: savedURL.path) { try! FileManager.default.removeItem(at: savedURL) }
+                try FileManager.default.moveItem(at: location, to: savedURL)
+                DispatchQueue.main.async {
+                    print("\((fileName == nil) ? url.lastPathComponent : fileName!) download successfully")
+                }
+            }
+        } catch {print ("file error: \(error)")}
+    }
+}
---
+++ BackgroundDownloadIOS/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  BackgroundDownloadIOS
+//
+//  Created by AMIT on 3/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let originalName = "file.mp4"
+    let filepath = "https://dr.sf-converter.com/download?id=b8e07d3e15e188f50dc7c2df0839705f9c31a9bcf1bced726cc32d9792cceb21&payload=1*eJzVVclu4zgQ%2FZXAQPPUSrSRkgIIDVnyIjt2vMTxcjG00BJt7WvkRv69KTkz6cHMpQdz6AGER4rFqmK94pO%2B9%2FK4zBy8yYLeY88viiR%2FfHio6%2Fq%2BicuitPG9E4cPtVU4%2FrdKleri5B%2FqJnOc3tcPT9P9ZUerdEn814xZhhiGySPmdLaYFJ6Dey%2BOvQBXxMVxF6qbJYHV2JZz%2BYbfEpJhlUMikjgZchzARL3CS7jPStPY85irp%2FMYYq3vAZKonMLfcyJ7z0M6yiIgrhoz2kyarV7OeIm06zVN5r7tbFi2OM2lYWNCjRiFmbwSnBB33QBSWJ5KQ4Bb1epHlSDDaUlPkueB2uAchL76egBhqArcF17nFRBG6k9V0TX65kMo5VEugTBXrZKuZW4JwkoNKRAVgSRQeRF4TqbaLiARKZw6cu0kp%2BXysgRZUCVZpXIgJCFWOzK%2F8MMwEUGUq%2FFAH%2B9qpxSe1pfsWm6bcuKVh8EIeCTrTugEOFIFyCIWKsAtM5Xn0D1UIAjCokvAcTJSZIXjeMpqtyYipLREg1NFuYTggnFiBaTCXcAT7QU9L8tygiS0RdOpxIsIOOp20AcFtUIo8KJAz6fKIsw0ITrOTXd0XYI8sTKLsnBrJ3XGhAJJWnBboKy3nHWct0T9yTZ9oQRRbLmgQ8sFHaK8NZDW0BZKB1oiRVocCP7IFvqtQ9hCuyNsfcIuSNWmT9rgP9FOHYmnaiNBP75p9Wpp%2Btp0B9HVgHCyGi2dzWq1fx2yITT2ROTdBnGCs%2FEaffekxQNDI%2F0Lt31KRObEjxP34Di2D2cWa4%2FTZPiy0xZmbW1fbJKkm02y%2FCIY9AFdwueUXU7keqWZ3nMzuNbn2oid10k45t%2FGA1SW263CzyCx8JFcz%2Bun8T50zKmmm9p0dTqhYW4k8IwsJOg2GsW%2BbFeeM53t5wUuq8WJ35fecUXF2OnqfylGAQGrnVFdCO3F4wSxQ9gh6lBqEbHdteQ7vF1RscPWyksyRUERO4Qdog6lDm9W5bfTfdeCX9U9J8sSbZL0KXxW%2BhQ%2BXaA2Fkmi8t8JH%2F4r4d8a%2B%2FtJ37N3zirNxs8vxDZeGH2oFOQ5XR52plvowqCRTlhoJH13ZmVZN5d6Km0Wtauc7cpcL041trSR7x2ZgYtTQ4pqjN3CXZustHz6R%2BnThLOjz5gLZlJWhoQy61gpB36gsOd5PbmMF67dN0S%2FDtJtiGc0oTFQArNGh%2FRshjLmsqmUSSHvnOBmMrTqcWBvR96OGQzJm%2FaRkH4CClIEmOrfDC2PRPjOyCwvjvI75q6PA4IrnNFNt07lWtF7%2FBT61x7J9biMiqzpk8glkdd7LLISf%2Fzj113zeo%2Ffe207aQYq%2B977xzfn70YB9d7ffwBRaLvP*1646707573*6092369d4ddfd79a"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+    @IBAction func downloadEvent(_ sender: Any) {
+        DownloadManager.shared().startDownload(filepath, originalName)
+    }
+}
+
